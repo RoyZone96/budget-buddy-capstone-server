87b8a704e33d3736c16a30c85940275b1b4da3fc
Briceson Roy <royzone3@ymail.com>
2021-03-12 17:41:24 -0500

    added URL Base

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/budget-buddy-capstone-server',
+    API_BASE_URL: 'https://budget-buddy-capstone-server.herokuapp.com/',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 }
